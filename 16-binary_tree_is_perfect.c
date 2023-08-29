e1b0da3c839473dc954cf51d1148c44874f290e2
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-08-29 06:20:27 +0100

    task_18

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,54 @@
+#include <stddef.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is perfect, 0 otherwise or if tree is NULL
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t height = binary_tree_height(tree);
+	size_t nodes = binary_tree_size(tree);
+
+	if (tree == NULL)
+		return (0);
+
+	size_t max_nodes = (1 << (height + 1)) - 1;
+
+	return (nodes == max_nodes);
+}
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: Size of the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: Height of the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Finds the sibling of a node
+ * @node: Pointer to the node to find the sibling
+ *
+ * Return: Pointer to the sibling node, or NULL if node is NULL, has no sibling,
+ *         or its parent is NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else if (node->parent->right == node)
+		return (node->parent->left);
+
+	return (NULL);
+}
