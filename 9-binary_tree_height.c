c65afc3e235e6646ba7186727da0fe2b061f286b
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-08-29 23:50:05 +0100

    task_9 fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,6 +14,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	if (tree->left == NULL &&  tree->right == NULL)
+		return (0);
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
